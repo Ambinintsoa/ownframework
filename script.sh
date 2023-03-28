f5905807673cc8f30aa1931473542e4a23606d35
Ambinintsoa <118436627+Ambinintsoa@users.noreply.github.com>
2023-03-28 11:07:16 +0300

    rectification de code

--- script.sh
+++ script.sh
@@ -1,10 +1,10 @@
 export CLASSPATH=.:/home/isabelle/jar/servlet-api.jar
 source ~/.bashrc
-cd FRAMEWORK
+cd FRAMEWORK/src
 javac -d . *.java 
-jar -cvf ../TEST_FRAMEWORK/WEB-INF/lib/etu1864.jar etu1864
-cp ../TEST_FRAMEWORK/WEB-INF/lib/etu1864.jar /home/isabelle/jar/
-cd ../TEST_FRAMEWORK/WEB-INF/classes
+jar -cvf ../../TEST_FRAMEWORK/WEB-INF/lib/etu1864.jar etu1864
+cp ../../TEST_FRAMEWORK/WEB-INF/lib/etu1864.jar /home/isabelle/jar/
+cd ../../TEST_FRAMEWORK/WEB-INF/classes
 export CLASSPATH=.:/home/isabelle/jar/etu1864.jar
 source ~/.bashrc
 javac -d . *.java 
