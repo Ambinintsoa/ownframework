075be6f8a6aa1f1f7c65f6066e9fa76b72352e06
Ambinintsoa <118436627+Ambinintsoa@users.noreply.github.com>
2023-04-04 11:31:11 +0300

    send data from model to view

--- TEST_FRAMEWORK/WEB-INF/classes/Emp.java
+++ TEST_FRAMEWORK/WEB-INF/classes/Emp.java
@@ -1,17 +1,27 @@
 
 package olona;
+import java.util.HashMap;
+import java.util.List;
+
 import etu1864.annotation.Urls;
 import etu1864.framework.ModelView;
-
+import java.util.ArrayList;
 public class Emp {
     private String name;
     
     public Emp() {
     }
+    public Emp(String name) {
+        this.name = name;
+    }
 
     @Urls(name = "/ddd")
     public ModelView bidon(){
         ModelView v = new ModelView();
+        v.setData(new HashMap<String,Object>());
+        ArrayList<Emp> a = new ArrayList<Emp>();
+        a.add(new Emp("jean"));
+        v.addItem("dg",a);
         v.setView("emp.jsp");
         return v;
     }
